1c58a8e5d3b0c2c71647f3690d40bb36fe532cb5
karan <karan.ker@gmail.com>
2015-08-03 20:06:59 -0400

    ensured feeds match design

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,11 +2,26 @@ angular.module('feedme.controllers', [])
 
 .controller('AppCtrl', function($scope, $localStorage) {
   $scope.myFeeds = $localStorage.menu.feeds; // replace with service to access this
-  console.log(JSON.stringify($scope.myFeeds));
+})
+
+.controller('AllFeedsCtrl', function($scope) {
+  $scope.myFeeds = $localStorage.menu.feeds;
 })
 
 .controller('FeedCtrl', ['$scope', 'FeedService', '$stateParams', function($scope, Feeds, $stateParams) {
   Feeds.loadFeed($stateParams.rss, 50).then(function(response) {
     $scope.feed = response;
+    console.log(response);
   });
+
+  $scope.getArticleDescription = function(description) {
+    var txt = document.createElement("textarea");
+    txt.innerHTML = String(description).replace(/<[^>]+>/gm, '');
+    var textValue = txt.value;
+    return textValue;
+  };
+
+  $scope.getArticleImage = function(description) {
+
+  }
 }]);
